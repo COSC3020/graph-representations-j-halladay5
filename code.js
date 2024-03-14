31323bc83399294a6088d1f97fb597d6e54c226e
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-03-14 12:32:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
+
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+	var adjList = [];
+    
+	for (var x = 0; x < adjMatrix.length; x++)
+    {
+	    adjList[x] = [];
+        
+		for (var y = 0; y < adjMatrix[x].length; y++) 
+        {
+			if (adjMatrix[x][y] == 1) 
+            {
+				adjList[x].push(y);
+			}
+		}
+	}
+	return adjList;
 }
